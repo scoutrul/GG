74ee802c200219bb784f68ea68b92891cb3e721d
Антон <scouturl@mail.ru>
2017-07-28 19:45:52 +0300

    choose location screen toggle

--- src/script/script.js
+++ src/script/script.js
@@ -1,27 +1,41 @@
 $(document).ready(function() {
 
-	let menuCondition = false;
+	let Hamburger = false;
+	let Choose = false;
 
+	// toggle hamburger
 	$('#hamburger').click(function() {
-		if (menuCondition) {
-			menuCondition = !menuCondition;
+		if (Hamburger) {
+			Hamburger = !Hamburger;
 			$(this).removeClass('is-active');
 			$('section.header').removeClass('mobile-menu-active')
 
 		} else {
-			menuCondition = !menuCondition;
+			Hamburger = !Hamburger;
 			$(this).addClass('is-active');
 			$('section.header').addClass('mobile-menu-active')
 		}
 	})
 
+	// if not index
 	if ($('section.hero')[0]) {
 		$('section.header').addClass('white_header');
 		console.log('white')
 	}
 	else {
 		console.log('black')
-
 	}
 
+	// toggle location
+	$('.location_toggle').click(function() {
+		if (Choose) {
+			Choose =!Choose;
+			$('#choose').removeClass('is-active');
+		}
+		else {
+			Choose =!Choose;
+			$('#choose').addClass('is-active');
+		}
+
+	})
 });
